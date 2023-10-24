9bc80d98b0832c4737564c48ae4d22d68d29485f
RafalRogalski <rogalski.rafal@outlook.com>
2023-10-24 19:02:36 +0200

    Added variables.py

---
+++ resources/variables.py
@@ -0,0 +1,5 @@
+class Variables:
+    """Class that stores configuration and variables that are being used in tests"""
+    ATTACH_SCREENSHOTS = False  # This variable allows adding screenshot after every element is found -> True/False
+
+    LUMA_TEST_SHOP_URL = "https://magento.softwaretestingboard.com/men.html"
