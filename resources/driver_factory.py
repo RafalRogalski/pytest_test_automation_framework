5134c82eae0f897cbbb1e1e9f8c69c33ad4e20d1
RafalRogalski <rogalski.rafal@outlook.com>
2023-10-24 19:02:45 +0200

    Added driver_factory.py

---
+++ resources/driver_factory.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class DriverFactory:
+    _driver = None
+
+    @staticmethod
+    def get_driver():
+        return DriverFactory._driver_generator()
+
+    @staticmethod
+    def _driver_generator():
+        if not DriverFactory._driver:
+            options = webdriver.ChromeOptions()
+            options.add_argument('--window-size=1600,1000')
+            options.add_argument('--incognito')
+            options.add_argument("--headless")
+
+            DriverFactory._driver = webdriver.Chrome(
+                service=ChromeService(ChromeDriverManager().install()),
+                options=options)
+
+        return DriverFactory._driver
